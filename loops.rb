bb11ba1e33a9f6c5691303642bf9682edb94071f
valeria farfan <valeria073104@gmail.com>
2018-06-05 20:48:23 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -1,7 +1,23 @@
-loop do
-  puts "to the right,to the right"
-  puts "to the left,to the left"
-  puts "now kick,kick"
-  puts "now walk it out"
-  break
-end
+#loop do
+#puts "to the right,to the right"
+#puts "to the left,to the left"
+#puts "now kick,kick"
+#puts now walk it out"
+#break
+#end
+
+
+# def dance 
+#   moves completed = 8
+  
+#   loop do
+#     if moves_completed == 10 
+#       break
+#     end #ends if statement
+    
+#     puts "to the right,to the right"
+#     puts "to the left,to the left"
+#     puts "now kick,kick"
+#     puts "now walk it out"
+    
+#     moves-completed == 10puts ""
