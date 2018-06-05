2134658f0f47b152d677034109fd477e0c5a5f02
valeria farfan <valeria073104@gmail.com>
2018-06-05 20:54:55 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -21,3 +21,58 @@
 #     puts "now walk it out"
     
 #     moves-completed == 10puts ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def dance 
+  moves_completed == 0 
+  
+  while moves_completed < 10
+  puts "to the left,to the left"
+  puts "walk it out"
+ 
+  moves_copleted += 1 
+  #moves_completed = moves_completed + 1 
+  
+  
+  
+  end #end while
+
+end #end method 
+
+dance 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
